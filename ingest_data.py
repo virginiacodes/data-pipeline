861ace2961b576a0b142b7f2b2b4c33485833258
Virginie Helmreich <virginie.helmreich@xandertalent.com>
2023-10-31 20:35:29 +0000

    Updated ingest file

--- ingest_data.py
+++ ingest_data.py
@@ -2,6 +2,14 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import os
+import json
+import csv
+
+from cherrypicker import CherryPicker
+
+import sqlite3 as sl
+import sqlalchemy as sa
+from sqlalchemy import create_engine
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -12,36 +20,66 @@ from spotipy.oauth2 import SpotifyClientCredentials
 
 AUTH_URL = 'https://accounts.spotify.com/api/token'
 
-# POST
-auth_response = requests.post(AUTH_URL, {
-    'grant_type': 'client_credentials',
-    'client_id': os.environ.get("SPOTIPY_CLIENT_ID"),
-    'client_secret': os.environ.get("SPOTIPY_CLIENT_SECRET")
-})
+# Function that returns artists name and track name in dictionary form
+def get_tracks():
 
-# convert the response to JSON
-auth_response_data = auth_response.json()
+    auth_response = requests.post(AUTH_URL, {
+        'grant_type': 'client_credentials',
+        'client_id': os.environ.get("SPOTIPY_CLIENT_ID"),
+        'client_secret': os.environ.get("SPOTIPY_CLIENT_SECRET")
+    })
 
-# save the access token
-access_token = auth_response_data['access_token']
+    # convert the response to JSON
+    auth_response_data = auth_response.json()
 
-headers = {
-    'Authorization': 'Bearer {token}'.format(token=access_token)
-}
+    # save the access token
+    access_token = auth_response_data['access_token']
 
-# base URL of all Spotify API endpoints
-BASE_URL = 'https://api.spotify.com/v1/'
+    headers = {
+        'Authorization': 'Bearer {token}'.format(token=access_token)
+    }
 
-# Get playlist data
-playlist_id = '1HXkVtgsiWJIZ4frLZV4hx'
+    # base URL of all Spotify API endpoints
+    BASE_URL = 'https://api.spotify.com/v1/'
 
-# pull all artists albums
-r = requests.get(BASE_URL + 'playlists/' + playlist_id + '/tracks', 
-                headers=headers,                  
-                params={'fields': 'items(track(name, artists(name, genres)))', 'limit': 100})
-d = r.json()
+    # Get playlist data
+    playlist_id = '1HXkVtgsiWJIZ4frLZV4hx'
 
-print(d)
+    # pull all playlist entries
+    r = requests.get(BASE_URL + 'playlists/' + playlist_id + '/tracks', 
+                    headers=headers,                  
+                    params={'fields': 'items(track(name, artists(name)))', 'limit': 100})
+    d = r.json()
 
-# name, artist[name], release_date
 
+    picker = CherryPicker(d)
+    flat = picker['items'].flatten().get()
+    df = pd.DataFrame(flat)
+
+
+    df = df.drop(['track_artists_1_name'], axis=1)
+    df = df.drop(['track_artists_2_name'], axis=1)
+    df = df.drop(['track_artists_3_name'], axis=1)
+    df = df.drop(['track_artists_4_name'], axis=1)
+    df = df.drop(['track_artists_5_name'], axis=1)
+
+
+
+    print(df)
+
+
+    # df = pd.DataFrame.from_dict(d, orient='columns')
+
+
+
+     
+
+    
+
+
+
+
+
+
+if __name__ == "__main__":
+    get_tracks()
